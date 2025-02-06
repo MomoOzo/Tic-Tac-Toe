ffb5b2149e5cd13a0c0700275a19def372b1c979
Mahmoud Abdel salam <mahmoudabdelsalam.alpha@gmail.com>
2025-02-06 16:09:21 +0200

    tic-tac-toe-game

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
-import Log from "./components/log";
+import Log from "./components/Log.jsx";
 import GameOver from "./components/GameOver";
 import { WINNING_COMBINATIONS } from "../winning-combination";
 
